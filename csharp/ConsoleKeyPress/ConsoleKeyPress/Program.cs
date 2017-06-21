fe2c74686993e791af7df05fce3e9f1799941441
Blogueira <blogueira@blogueiraarteira.com.br>
2017-06-21 01:56:32 -0300

    nada

---
+++ csharp/ConsoleKeyPress/ConsoleKeyPress/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleKeyPress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press ESC to stop");
+            do
+            {
+                while (!Console.KeyAvailable)
+                {
+                    Console.WriteLine(Console.ReadKey());
+                }
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+        }
+    }
+}
